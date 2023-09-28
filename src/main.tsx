5ab795b45eec86ba899dd2fb25c5926a68a0cb7c
lucifer1708 <hello@sumitdhiman.in>
2023-09-28 23:58:38 +0530

    backend and frontend integrated and tested

--- src/main.tsx
+++ src/main.tsx
@@ -3,19 +3,21 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 
+
 const layoutStyles = {
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "center",
-  alignItems: "center",
-  padding: "0%",
-  minHeight: "100vh",
+     display: "flex",
+     flexDirection: "column",
+     justifyContent: "center",
+     alignItems: "center",
+     padding: "0%",
+     minHeight: "100vh",
 };
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <div className="" style={layoutStyles as React.CSSProperties}>
-      <App />
-    </div>
-  </React.StrictMode>
+     <React.StrictMode>
+          <div className="bg-gradient-to-b from-ctp-base to-ctp-crust p-6 bg-fixed" style={layoutStyles as React.CSSProperties}>
+               <App />
+          </div>
+     </React.StrictMode>,
+
 );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,46 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    colors: {
-      primary: {
-        light: "#48A2AE",
-        dark: "#052536",
-      },
-      secondary: {
-        dark: "#9ED8FF",
-        light: "#A0c0c0",
-      },
-      background: {
-        light: "#F2F2F2",
-        dark: "#CCCCCC",
-      },
-      success: "#379253",
-    },
-    //     fontSize: {
-    //       sm: "0.8rem",
-    //       base: "1rem",
-    //       normal: "18px",
-    //       xl: "1.25rem",
-    //       "2xl": "1.563rem",
-    //       "3xl": "1.953rem",
-    //       "4xl": "2.441rem",
-    //       "5xl": "3.52rem",
-    //     },
-    fontFamily: {
-      sans: ["Poppins, sans-serif"],
-    },
-    screens: {
-      sm: "640px",
-      // => @media (min-width: 640px) { ... }
+     content: [
+          "./index.html",
+          "./src/**/*.{js,ts,jsx,tsx}",
+     ],
+     theme: {
+          fontSize: {
+               sm: '0.8rem',
+               base: '1rem',
+               normal: '18px',
+               xl: '1.25rem',
+               '2xl': '1.563rem',
+               '3xl': '1.953rem',
+               '4xl': '2.441rem',
+               '5xl': '3.52rem',
+          },
+          screens: {
+               'sm': '640px',
+               // => @media (min-width: 640px) { ... }
 
-      md: "768px",
-      // => @media (min-width: 768px) { ... }
+               'md': '768px',
+               // => @media (min-width: 768px) { ... }
 
-<<<<<<< HEAD
-      lg: "1024px",
-      // => @media (min-width: 1024px) { ... }
-=======
                'lg': '1024px',
                // => @media (min-width: 1024px) { ... }
 
@@ -59,6 +40,7 @@ export default {
           },
      },
      plugins: [
+          require('tailwind-scrollbar'),
           require("@catppuccin/tailwindcss")({
                defaultFlavour: "mocha",
                prefix: "ctp",
@@ -66,27 +48,5 @@ export default {
 
      ],
 }
->>>>>>> parent of 79854cc (backend done only dashboard UI left)
 
-      xl: "1280px",
-      // => @media (min-width: 1280px) { ... }
 
-      "2xl": "1536px",
-      // => @media (min-width: 1536px) { ... }
-    },
-    extend: {
-      width: {
-        "62": "62em",
-        "34": "34em",
-        "44": "40em",
-      },
-    },
-  },
-  plugins: [
-    require("tailwind-scrollbar"),
-    require("@catppuccin/tailwindcss")({
-      defaultFlavour: "mocha",
-      prefix: "ctp",
-    }),
-  ],
-};
