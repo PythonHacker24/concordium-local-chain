2713a6235922ab6fa2259ff3bd51098cb98b5ef4
Emil Lai <39825288+EmilLa1@users.noreply.github.com>
2024-01-16 08:53:18 +0100

    Update source for debian package.

--- src-tauri/src/views.rs
+++ src-tauri/src/views.rs
@@ -35,7 +35,7 @@ pub async fn install(_handle: tauri::AppHandle) -> Result<(), String> {
     } else if cfg!(target_os = "macos") {
         "https://distribution.concordium.software/macos/signed/concordium-node-6.2.3-0.pkg"
     } else if cfg!(target_os = "linux") {
-        "https://distribution.mainnet.concordium.software/deb/concordium-mainnet-node_6.0.4-0_amd64.deb"
+        "https://distribution.mainnet.concordium.software/deb/concordium-mainnet-node_6.2.3-0_amd64.deb"
     } else {
         return Err("Unsupported OS".into());
     };
